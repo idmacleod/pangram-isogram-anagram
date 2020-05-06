d266cc56e94d8d2cb8a36f38848cab3fd870e665
idmacleod <idmacleod@gmail.com>
2020-05-06 17:23:59 +0100

    Finished isogram_finder.js

--- isogram_finder/isogram_finder.js
+++ isogram_finder/isogram_finder.js
@@ -1,9 +1,14 @@
 const IsogramFinder = function (word) {
+    this.word = word.toLowerCase().split('');
+}
 
+IsogramFinder.prototype.letterAppearsOnce = function (letterToCheck) {
+    const letterArray = this.word.filter(letter => letter === letterToCheck);
+    return letterArray.length === 1;
 }
 
 IsogramFinder.prototype.isIsogram = function () {
-
+    return this.word.every(letter => this.letterAppearsOnce(letter));
 }
 
 module.exports = IsogramFinder;
