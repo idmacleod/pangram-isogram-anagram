3c78a120a3d4ed36c0d543057de6ba6344df4c43
idmacleod <idmacleod@gmail.com>
2020-05-06 18:02:54 +0100

    Finished anagram_finder.js

--- anagram_finder/anagram_finder.js
+++ anagram_finder/anagram_finder.js
@@ -1,9 +1,18 @@
 const AnagramFinder = function (word) {
+    this.word = word.toLowerCase();
+}
 
+AnagramFinder.prototype.wordIsAnagramOf = function (otherWord) {
+    if (otherWord.toLowerCase() === this.word || otherWord.length !== this.word.length) {
+        return false;
+    } else {
+        const letterArray = otherWord.toLowerCase().split('');
+        return letterArray.every(letter => this.word.includes(letter));
+    }
 }
 
 AnagramFinder.prototype.findAnagrams = function (otherWords) {
-
+    return otherWords.filter(otherWord => this.wordIsAnagramOf(otherWord));
 }
 
 module.exports = AnagramFinder;
