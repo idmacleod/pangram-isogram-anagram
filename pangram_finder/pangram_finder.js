0c5e5c5dfc47a014505794bb50a5f6649dcefce3
idmacleod <idmacleod@gmail.com>
2020-05-06 16:35:36 +0100

    Finished pangram_finder.js

--- pangram_finder/pangram_finder.js
+++ pangram_finder/pangram_finder.js
@@ -1,9 +1,13 @@
 const PangramFinder = function (phrase) {
   this.alphabet = 'qwertyuiopasdfghjklzxcvbnm'.split('');
+  this.phrase = phrase;
 }
 
 PangramFinder.prototype.isPangram = function () {
-
+  const lowerCasePhrase = this.phrase.toLowerCase();
+  return this.alphabet.reduce((result, letter) => {
+    return result && lowerCasePhrase.includes(letter);
+  });
 }
 
 module.exports = PangramFinder;
